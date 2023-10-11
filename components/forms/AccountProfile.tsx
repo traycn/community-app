65b6f8c60da0c7619bd2d760122a2355d6279ebd
traycn <tng.dev.env@gmail.com>
2023-10-11 14:48:07 -0400

    CREATE threads functionality.

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -145,6 +145,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
