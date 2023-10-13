3e0e828a35aa764014f6db961607c53df5d9d5f4
T N <tng.dev.env@gmail.com>
2023-10-13 00:57:45 -0400

    QF: Remove extra console.logs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,8 +8,6 @@ export default async function Home() {
 
   const result = await fetchThreads(1, 30);
 
-  console.log("fetchThreads: ", result);
-
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -13,9 +13,6 @@ async function Page({ params }: {  params: { id: string }}) {
   if(!user) return null;
 
   const userInfo = await fetchUser(params.id);
-
-  console.log("userInfo: ", userInfo);
-
   if(!userInfo?.onboarded) redirect('/onboarding');
   
   return (
