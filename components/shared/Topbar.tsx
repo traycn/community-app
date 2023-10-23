77641a80b2f7698d8bc1a9a9167c2d3f680a6741
T N <tng.dev.env@gmail.com>
2023-10-23 15:41:08 -0400

    Add UserNav component.

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,35 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
 import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import UserNav from "@/app/(auth)/components/shared/UserNav";
 
-function Topbar() {
-    return (
-        <nav className="topbar">
-            <Link href="/" className="flex items-cneter gap-4">
-                <Image src="/assets/community-app-logo.svg" alt="logo" width={28} height={28} />
-                <p className="text-heading3-bold text-light-1 max-xs:hidden">Community</p>
-            </Link>
+function Topbar () {
+  
+  return (
+      <nav className="topbar">
+          <Link href="/" className="flex items-center gap-4">
+              <Image src="/assets/community-app-logo.svg" alt="logo" width={28} height={28} />
+              <p className="text-heading3-bold text-light-1 max-xs:hidden">Community</p>
+          </Link>
 
-            <div className="flex items-center gap-1">
-                <div className="block md:hidden">
-                    <SignedIn>
-                        <SignOutButton>
-                            <div className="flex 
-                                cursor-pointer">
-                                    <Image
-                                        src="/assets/logout.svg"
-                                        alt="logout"
-                                        width={24}
-                                        height={24} 
-                                    />
-                            </div>
-                        </SignOutButton>
-                    </SignedIn>
-                </div>
-                
-            </div>
-        </nav>
-    )
+          <div className="flex items-center gap-1">
+              <div className="block md:hidden">
+                  <SignedIn>
+                    <SignOutButton>
+                        <div className="flex 
+                            cursor-pointer">
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24} 
+                                />
+                        </div>
+                    </SignOutButton>
+                  </SignedIn>
+              </div>
+          </div>
+          <UserNav />
+      </nav>
+  )
 }
 
 export default Topbar;
