15a7462e42ae2bb5fe12d2f760b849ad6af8f876
T N <tng.dev.env@gmail.com>
2023-10-23 16:54:50 -0400

    Update public routes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook/clerk'],
-    ignoredRoutes: ['/api/webhook/clerk'],
+    publicRoutes: ['/', '/sign-in', '/sign-up'],
+    ignoredRoutes: [],
 });
 
 export const config = {
