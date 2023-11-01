0bc76e89dec3cf3589b7040492cb28d6b8c3fcc2
T N <tn.eng@outlook.com>
2023-10-31 20:44:44 -0400

    Update middleware with public routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: [],
+  publicRoutes: ['/', '/sign-in', '/sign-up'],
 
   // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ["/api/uploadthing"],
