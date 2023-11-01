36fee6f35ff2bf06b394a7d47f219b711f719b80
T N <tn.eng@outlook.com>
2023-10-31 20:29:34 -0400

    Remove multiple redirect onboarding. Add redirect to homepage and usernav fallback.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
 import PostCard from "@/components/cards/PostCard";
 import { fetchPosts } from "@/lib/actions/post.actions";
-import { currentUser } from '@clerk/nextjs'
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
 
   const user = await currentUser();
+  if(!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if(!userInfo?.onboarded) redirect('/onboarding');
 
   const result = await fetchPosts(1, 30);
 
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -9,7 +9,6 @@ async function Page() {
   if(!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if(!userInfo?.onboarded) redirect('/onboarding');
 
   const result = await fetchUsers({
     userId: user.id,
